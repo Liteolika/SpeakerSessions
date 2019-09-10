1c6375e76f654f0fde4eee9f86d051f75f270b31
Peter Carlsson <peter.carlsson@activesolution.se>
2019-09-10 09:47:00 +0200

    added telldontask session with slides and demo

---
+++ Tell Dont Ask/Demo/DemoProject/ShortDemo.Refactored.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using Xunit;
+
+namespace DemoProject.Refactored
+{
+    public interface IAlerter { void Alert(string message); }
+
+    public class MemoryObject
+    {
+        private readonly int _threshold;
+        private readonly Action<string> _sendAlert;
+
+        public MemoryObject(int threshold, Action<string> sendAlert)
+        {
+            _threshold = threshold;
+            _sendAlert = sendAlert;
+        }
+
+        public int UsedPercentage { get; set; }
+        public bool ExceedsThreshold => UsedPercentage >= _threshold;
+
+        public void Sample()
+        {
+            if (ExceedsThreshold)
+                _sendAlert("Alert");
+        }
+    }
+
+    public class AlertSender
+    {
+        private readonly IAlerter _alerter;
+        public AlertSender(IAlerter alerter) { _alerter = alerter; }
+        public void SendAlerts(List<MemoryObject> memories)
+        {
+            foreach (var memory in memories)
+                memory.Sample();
+                
+        }
+    }
+
+    public class MemoryAlertTests
+    {
+        [Theory]
+        [InlineData(89, 90, false)]
+        [InlineData(90, 90, true)]
+        public void Sends_Alert_If_Above_Threshold(int percentage, int threshold, bool shouldSendAlert)
+        {
+            // Arrange
+            var sendAlert = Substitute.For<Action<string>>();
+            var memory = new MemoryObject(threshold, sendAlert) {UsedPercentage = percentage};
+
+            // Act
+            memory.Sample();
+
+            // Assert
+            sendAlert.ReceivedWithAnyArgs(
+                shouldSendAlert
+                    ? Quantity.Exactly(1)
+                    : Quantity.None());
+        }
+    }
+}
---
+++ Tell Dont Ask/Demo/DemoProject/ShortDemo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using Xunit;
+
+namespace DemoProject
+{
+    public interface IAlerter { void Alert(string message); }
+
+    public class MemoryObject
+    {
+        public int UsedPercentage { get; set; }
+    }
+
+    public class AlertSender
+    {
+        private readonly IAlerter _alerter;
+        public AlertSender(IAlerter alerter) { _alerter = alerter; }
+        public void SendAlerts(List<MemoryObject> memories)
+        {
+            foreach (var memory in memories)
+                if (memory.UsedPercentage >= 90)
+                    _alerter.Alert($"Memory usage is {memory.UsedPercentage}.");
+        }
+    }
+
+    public class MemoryAlertTests
+    {
+        [Theory]
+        [InlineData(89, false)]
+        [InlineData(90, true)]
+        public void Sends_Alert_If_Above_Threshold(int percentage, bool shouldSendAlert)
+        {
+            // Arrange
+            var alerter = Substitute.For<IAlerter>();
+            var memories = new List<MemoryObject>()
+            {
+                new MemoryObject() {UsedPercentage = percentage}
+            };
+            var alertSender = new AlertSender(alerter);
+
+            // Act
+            alertSender.SendAlerts(memories);
+
+            // Assert
+            alerter.ReceivedWithAnyArgs(
+                    shouldSendAlert
+                        ? Quantity.Exactly(1)
+                        : Quantity.None())
+                .Alert(string.Empty);
+        }
+    }
+}
